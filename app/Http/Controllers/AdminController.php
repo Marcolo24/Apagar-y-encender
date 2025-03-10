c40135dd8282eec9c98cd066115290b0d4380716
DavidHompaneraCampos <davidhompaneracampos@gmail.com>
2025-03-10 17:21:59 +0100

    inicioLogin

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\Restaurant;
+use App\Models\User;
+use App\Models\Role;
+use App\Models\Location;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+
+    }
+}
